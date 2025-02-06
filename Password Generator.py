5289daef26a0724a58fff2ed59c656a2851cf72e
Danvick Bidaha <danvickmak@gmail.com>
2025-02-06 23:48:01 +0300

    Add files via upload

---
+++ Password Generator.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+def generate_password(length):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(chars) for _ in range(length))
+    print(password)
+
+generate_password(12)
